5e725e76e104a569b1b294bb7649d9902a1dd6e9
cihanb <cihanb@live.com>
2019-06-20 22:13:38 +0300

    added compression

--- restore_sample_edm.sh
+++ restore_sample_edm.sh
@@ -9,14 +9,13 @@ docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SA12#45678" -p 1433:1433 --name sq
 
 # STEP 2
 # Copy backup file
+unzip ./EDM_Test_Orig.bak.zip
 docker exec -it sql1 mkdir /var/opt/mssql/backup
 docker cp ./EDM_Test_Orig.bak sql1:/var/opt/mssql/backup
-
+rm -f ./EDM_Test_Orig.bak
 # output should indicate 2 files
 # RMS_EDM_YYTest D:\AttachedDatabases\RMS_EDM_YYTest.mdf
 # RMS_EDM_YYTest_log D:\AttachedDatabases\RMS_EDM_YYTest_log.LDF
 
 # STEP 3
 docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'SA12#45678' -Q 'RESTORE DATABASE db1 FROM DISK = "/var/opt/mssql/backup/EDM_Test_Orig.bak" with REPLACE, MOVE "RMS_EDM_YYTest" to "/var/opt/mssql/RMS_EDM_YYTest.mdf", MOVE "RMS_EDM_YYTest_log" to "/var/opt/mssql/RMS_EDM_YYTest.ldf"'
-
-
